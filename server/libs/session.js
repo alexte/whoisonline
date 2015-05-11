23e8d31e96834361fdbe3050f003957a9de3cf42
Alexander Terczka <alex@mail.at>
2015-05-11 19:19:49 +0200

    session handling and login form for webclient implemented

---
+++ server/libs/session.js
@@ -0,0 +1,49 @@
+/*
+ * Web/Cookie Session Handling for whoisonline webclient
+ */
+
+var uid = require('uid-safe');
+var cookie_name = "wio_sess";
+var debug = 0;
+
+module.exports = function()
+{
+    var sesslist={};
+
+    // sets a session when user logges in
+    this.init_session = function(req,res)
+    {
+        var id=uid.sync(20);
+        res.cookie(cookie_name,id);
+        sesslist[id]={ "id":id, authenticated:true };
+        req.session=sesslist[id];
+        return id;
+    };
+
+    // express middleware to check if user has a session allready
+    this.check_session = function(req,res,next)
+    {
+        req.session={ authenticated: false };
+
+        if(req.cookies && req.cookies[cookie_name])
+        {
+            if (sesslist[req.cookies[cookie_name]]) req.session=sesslist[req.cookies[cookie_name]];
+            else { res.clearCookie(cookie_name); }
+        }
+        next();
+    };
+
+    // remove session by session_id or session object
+    this.remove_session = function(o)
+    {
+        if (!o) return false;
+        if (sesslist[o]) { delete sesslist[o]; return true; }
+        if (o.id && sesslist[o.id]) { delete sesslist[o.id]; return true; }
+        return false;
+    };
+   
+    if (debug) console.log("modules sessions loaded");
+
+    return this;
+}
+
--- server/server.js
+++ server/server.js
@@ -1,12 +1,59 @@
+/*
+ * WhoisOnline Node Server
+ */
 
 var express = require('express');
 var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-// ---- static webclient files reachable at /webclient
-app.use('/webclient', express.static('../webclient'));
+var Session = require('./libs/session.js');
+var session = new Session();
 
-app.get('/', function(req,res) {
-    res.send("Hello World!");
+// ------ clientapi functions
+function ca_login(req,res)
+{
+    var login_ok=false;
+
+    if (req.body.username && req.body.password)
+    {
+	if (req.body.username==req.body.password)  // TODO: real login check
+	    login_ok=true;
+    }
+   
+    // check login password
+    if (login_ok) 
+    {
+	session.init_session(req,res);
+	res.send({result:200, data:"OK"});
+    }
+    else res.send({result:401, data:"username or password wrong"});
+		// TODO: throttleing for login attempts
+}
+
+// ---- express middleware modules
+app.use(cookieParser());
+
+app.use(session.check_session);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all("/clientapi/:cmd",function (req,res) { 
+    if (!req.params.cmd) { res.send({ result:404, data: 'missing command'}); return; }
+    if (req.params.cmd=="login") { ca_login(req,res); return; }
+    if (!req.session.authenticated) { res.send({ result:401, data: 'authentication needed'}); return; }
+    res.send({ result:404, data: 'unknown command'}); 
+});
+
+app.get("/",function (req,res) { 
+    if (!req.session.authenticated) res.redirect("/login");
+    else res.sendFile(path.resolve("../webclient/index.html"));
+});
+
+app.get("/login",function (req,res) { 
+    res.sendFile(path.resolve("../webclient/login.html")); 
 });
 
 var server = app.listen(8042, function () {
