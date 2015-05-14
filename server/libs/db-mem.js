ee6fe3faea36e91b9dd3f4af50df1a7c8c94940d
Alexander Terczka <alex@mail.at>
2015-05-14 21:02:24 +0200

    start of the sample db (mem) module

---
+++ server/libs/db-mem.js
@@ -0,0 +1,24 @@
+/*
+ * in memory database for whoisonline sever
+ *
+ */
+
+function db()
+{
+    var users={};
+ 
+    var messages=[];
+
+    this.check_login_password=function(login,password)
+    {
+    }
+    
+    this.get_conversations=function(user)
+    {
+    }
+
+    this.get_messages=function(user,selector)
+    {
+    }
+}
+
