1a8a43978b23bd7b4c45e5745c7bf11b674b9f5b
Alexander Terczka <alex@mail.at>
2015-05-18 19:45:27 +0200

    usernames without domain get the first domain of the domain list in the config file

--- server/libs/db-mem.js
+++ server/libs/db-mem.js
@@ -14,7 +14,15 @@ module.exports = function (full_config)
 	// calls callback with false if login/password is not ok
     this.check_login_password=function(login,password,callback)
     {
-	if (config.auth_method=='dummy' && login.length>=3 && login==password) callback({ username:login });
+	var username; 
+
+	if (login.indexOf("@")==-1)
+	{
+	    if (full_config.domains && full_config.domains[0].length>1) username=login+"@"+full_config.domains[0];
+	    else { callback(false); return; }
+	}
+	else username=login;
+	if (config.auth_method=='dummy' && login.length>=3 && login==password) callback({ username:username });
 	else callback(false);
     }
     
